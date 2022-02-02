bd3bcc856a95d8570117943b8bda97d3373be918
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-02-02 05:28:42 +0000

    Added: Main class Linkedlist

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LinkedListApproach
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList Approach ");
+            SinglyLinkedlist sl = new SinglyLinkedlist();
+            sl.InsertFront(5);
+            sl.InsertLast(7);
+            sl.InsertLast(10);
+            sl.InsertLast(17);
+            sl.InsertLast(25626);
+            sl.InsertLast(2452);
+            sl.InsertLast(41252);
+
+
+            sl.SwapNode(17, 41252);
+
+           // sl.display();
+           //  Console.WriteLine("list deleted ");
+           //  sl.DeleteWholelist();
+           // Console.WriteLine("list deleted");
+            sl.display();
+        }
+    }
+}
